ad3a08b541e8522edf79ca5c57379a2c1370a736
faustine-van <faustinemuhayemariya44@gmail.com>
2023-08-05 20:54:56 +0000

    updated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,25 +1,4 @@
 #!/usr/bin/env ruby
 
 
-def extract_info(text)
-	re_from = /\[from:(.*?)\]/
-	re_to = /\[to:(.*?)\]/
-	re_flags = /\[flags:(.*?)\]/
-
-	f = text.scan(re_from)
-  	t = text.scan(re_to)
-  	fl = text.scan(re_flags)
-
-  	from = f.flatten.join(", ")
-  	to = t.flatten.join(", ")
-  	flags = fl.flatten.join(", ")
-
-  	puts "#{from}, " unless from.empty?
-  	puts "#{to}, " unless to.empty?
-  	puts "#{flags}" unless flags.empty?
-end
-
-text = ARGV[0]
-extract_info(text)
-
-
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
