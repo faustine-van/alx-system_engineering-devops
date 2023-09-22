74bcc41f9ff6dd764061d736838abdfd6c2e4aae
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-22 18:59:25 +0000

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
 
 file { '/tmp/school':
-  ensure => 'file',
+  ensure  => 'file',
   content => 'I love Puppet',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
