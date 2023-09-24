e1d7632bca6c881aecb971bb56adeb7d9e494257
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-24 09:35:39 +0000

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,17 +2,25 @@
 # authentication
 
 class shhConfig {
-  $ssh_config_file = '/etc/ssh/ssh_config'
+  file { '/etc/ssh/ssh_config' :
+    ensure => 'file',
+  }
 
-  $config_content = "
-    IdentityFile ~/.ssh/id_rsa
-    PasswordAuthentication yes
-  "
-
-  file { $ssh_config_file:
-    ensure  => 'present',
-    content => $config_content,
+  # Ensure SSH client uses the specified private key
+  file_line { 'IdentityFile':
+    path  => '/etc/ssh/ssh_config',
+    line  => '    IdentityFile ~/.ssh/school',
+    match => '^#?\s*IdentityFile\s.*$',
+  }
+  # disable password authentication in ssh configuration
+  file_line { 'disable password':
+    path  => '/etc/ssh/ssh_config',
+    line  => '    PasswordAuthentication no',
+    match => '^#?\s*PasswordAuthentication\s.*$',
+  }
+  # notify any changes made
+  notify { 'notify change':
+    require => File['/etc/ssh/ssh_config'],
   }
 }
-
 include shhConfig
