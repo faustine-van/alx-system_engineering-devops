f4aa8bac4623de92db142e06522b8e530b08cc41
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-24 07:38:05 +0000

    updated

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# # Configure SSH client for key-based authentication and disable
+# password authentication
+
+class shhConfig {
+  $ssh_config_file = '/etc/ssh/ssh_config'
+
+  $config_content = "
+    IdentityFile ~/.ssh/id_rsa
+    PasswordAuthentication yes
+  "
+
+  file { $ssh_config_file:
+    ensure  => 'present',
+    content => $config_content,
+  }
+
+}
