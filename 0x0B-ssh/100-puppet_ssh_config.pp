aab44eaaa65a331ddc6d3e972a0d62761d7b6746
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-24 11:56:11 +0000

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,29 +1,25 @@
 # - Configure SSH client for key-based authentication and disable password
 # authentication
 
-class shhConfig {
-  file { '/etc/ssh/ssh_config' :
-    ensure => 'file',
-  }
 
-  # Ensure SSH client uses the specified private key
-  augeas { 'IdentityFile':
-    context => "/files/etc/ssh/ssh_config",
-    changes => ["set Host[.='*']/IdentityFile '~/.ssh/school'",],
-    # notify  => Service['ssh.service '],  # Restart SSH service for changes
-  }
-  # disable password authentication in ssh configuration
-  augeas { 'disable password':
-    context  => '/files/etc/ssh/ssh_config',
-    changes  => "set Host[.='*']/PasswordAuthentication no",
-    # notify  => Service['ssh.service '],  # Restart SSH service for changes
-  }
-  # notify any changes made
-  notify { 'SSH client configuarion updated':
-    require => [
-     File['/etc/ssh/ssh_config'],
-     Augeas['disable password'],
-     Augeas['disable password'],],
-  }
+# Ensure SSH client uses the specified private key
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
 }
-include shhConfig
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
