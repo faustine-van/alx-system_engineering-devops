82e2d2c72cc1e5726081c69aa5dc6bc3bc880cf8
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-26 21:36:34 +0000

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,55 @@
-# Install Nginx web server and configure puppet
-
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
+# Install Nginx
+class { 'nginx':
+  ensure => 'installed',
 }
 
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt-get update'],
+# Create a custom Nginx configuration for the redirect
+file { '/etc/nginx/sites-available/redirect_config':
+  ensure => file,
+  content => 'server {
+    listen 80;
+    server_name localhost;
+
+    location / {
+      root /var/www/html;
+      index index.html;
+    }
+
+    location /redirect_me {
+      rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+  }',
+  require => Class['nginx'],
 }
 
-exec { 'list to port':
-  path    => 'usr/bin:/bin',
-  command => 'echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html',
-  require => Package['nginx'],
+# Activate the custom Nginx configuration by creating a symbolic link
+file { '/etc/nginx/sites-enabled/redirect_config':
+  ensure => link,
+  target => '/etc/nginx/sites-available/redirect_config',
+  require => File['/etc/nginx/sites-available/redirect_config'],
+  notify => Exec['nginx_reload'],
 }
 
-$string = "\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"
-
-exec { '/redirect_me is redirecting to another page':
-  path    => 'usr/bin:/bin',
-  command => 'sudo sed -i "40i $string" /etc/nginx/sites-available/default',
-  require => Exec['list to port'],
+# Ensure Nginx is running and listening on port 80
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  hasstatus => true,
+  hasrestart => true,
 }
 
-exec { 'append to the default file':
-  path    => 'usr/bin:/bin',
-  command => 'sudo service nginx restart',
-  require => Exec['/redirect_me is redirecting to another page'],
+# Create the index.html file with "Hello World!" in /var/www/html
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Class['nginx'],
 }
+
+# Notify Nginx to reload the configuration when the redirect_config file changes
+exec { 'nginx_reload':
+  command     => 'service nginx reload',
+  path        => '/usr/bin:/bin',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/sites-enabled/redirect_config'],
+}
+
