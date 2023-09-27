6881c5d0e379e6cc9e457f32ddc02ee8b1795ff6
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-27 11:59:45 +0000

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,5 @@
 # Install Nginx web server and configure puppet
 
 exec { 'apt-get update':
-  path    => ['/usr/bin', '/bin'],
-  command => 'apt-get update',
-}
-
-package { 'nginx':
-  ensure          => 'installed',
-  provider        => 'apt',
-  install_options => ['-y'],
-  require         => Exec['apt-get update'],
-}
-
-
-file { '/var/www/html/index1.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-}
-
-exec { 'redirect_me':
-  command => 'sudo sed -i "40i\		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/defaul',
-  provide => 'shell',
-}
-exec { 'restart':
-  path    => 'usr/bin:/bin',
-  command => 'sudo service nginx restart',
+  command => '/usr/bin/apt-get update',
 }
