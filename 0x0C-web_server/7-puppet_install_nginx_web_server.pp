e790c20295ee65a61f49dc5b40292c2b0e44cd45
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-27 09:05:26 +0000

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,28 +16,22 @@ file { '/var/www/html/index1.html':
   ensure  => 'file',
   content => 'Hello World!',
 }
+file { '/var/www/html/index1.html':
+  ensure  => 'file',
+  content => 'server {
+    listen 80;
+    server_name localhost;
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => '
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    location / {
+      root /var/www/html;
+      index index.html;
+    }
 
-        # Add index.php to the list if you are using PHP
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files $uri $uri/ =404;
-        }
-}
-',
+    location /redirect_me {
+      rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+  }',
+  require => Package['nginx'],
 }
 
 exec { 'restart':
