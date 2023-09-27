c528f524b537f6986deb0cefcab3766c78de0af1
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-27 11:58:04 +0000

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,6 +9,7 @@ package { 'nginx':
   ensure          => 'installed',
   provider        => 'apt',
   install_options => ['-y'],
+  require         => Exec['apt-get update'],
 }
 
 
@@ -17,7 +18,10 @@ file { '/var/www/html/index1.html':
   content => 'Hello World!',
 }
 
-
+exec { 'redirect_me':
+  command => 'sudo sed -i "40i\		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/defaul',
+  provide => 'shell',
+}
 exec { 'restart':
   path    => 'usr/bin:/bin',
   command => 'sudo service nginx restart',
