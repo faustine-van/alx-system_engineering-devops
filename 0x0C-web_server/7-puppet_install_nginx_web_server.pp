63eb4f4de7eebc979ed659d76b9365aef936eacb
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-27 11:47:36 +0000

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -18,7 +18,7 @@ file { '/var/www/html/index1.html':
 }
 
 exec { 'configure redirect_me':
-  path     => ['usr/bin','/bin]',
+  path     => ['usr/bin','/bin'],
   command  => 'sudo sed -i "40i \\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites_available/default',
   provider => 'shell',
 
