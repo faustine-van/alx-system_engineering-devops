d5646e5a90c59ade0f0aa65c88fc03c92e76ab39
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-27 12:01:31 +0000

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,3 +3,10 @@
 exec { 'apt-get update':
   command => '/usr/bin/apt-get update',
 }
+
+package { 'nginx':
+  ensure          => 'installed',
+  provider        => 'apt',
+  install_options => ['-y'],
+  require         => Exec['apt-get update'],
+}
