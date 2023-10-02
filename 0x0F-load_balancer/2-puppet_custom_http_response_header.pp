6292b5df367a4eb37709aaff5e8cf7ee49a83dcf
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-02 20:30:31 +0000

    push

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,5 @@
 # automate the task of creating a custom HTTP header response, but with Puppet.
 
-exec { 'update':
-  command => '/usr/bin/apt-get update',
-}
-
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update'],
-}
-
-file { '/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-}
-exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
-
 exec { 'X-Served-By':
   command  => 'sed -i "42i	add_header X-Served-By $\hostname;" /etc/nginx/sites-available/default',
   provider => 'shell',
