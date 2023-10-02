58ece0ecfe6885531a3c2a0bd7d9016004725d68
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-02 21:41:43 +0000

    updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,24 +9,6 @@ package { 'nginx':
   require => Exec['update'],
 }
 
-file { '/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-}
-exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "Ceci n'est pas une page",
-}
-$str     => 'error_page 404 /page_404.html;\n\tlocation = /page_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}"'
-exec { 'configre 404-page':
-  command  => 'sed -i "41i	$str /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
 exec { 'X-Served-By':
   command  => 'sed -i "42i	add_header X-Served-By $\hostname;" /etc/nginx/sites-available/default',
   provider => 'shell',
