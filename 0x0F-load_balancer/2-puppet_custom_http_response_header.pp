f074d0e90d77f8e29c1eb77ee22c5bea8c24ad7e
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-02 13:01:37 +0000

    0-custom_http_response_header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
-exec {'custom HTTP header response':
-  command  => 'sudo sed -i "/server_name _;/a\\\tadd_header X-Served-By $\hostname;" /etc/nginx/sites-available/default'
+exec { 'custom HTTP header response':
+  command  => 'sudo sed -i "/server_name _;/a\\\tadd_header X-Served-By $\hostname;" /etc/nginx/sites-available/default',
   provider => shell,
 }
 
-service {'nginx':
+service { 'nginx':
   ensure  => 'running',
-  require => Package['nginx'].
+  require => Package['nginx'],
 }
