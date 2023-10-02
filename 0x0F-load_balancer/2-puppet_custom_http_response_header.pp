2abcb193748df89e0f205fe8c5960221402e94f1
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-02 13:08:15 +0000

    updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,27 @@
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update'],
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell',
+}
+
 exec { 'custom HTTP header response':
   command  => 'sudo sed -i "/server_name _;/a\\\tadd_header X-Served-By $\hostname;" /etc/nginx/sites-available/default',
   provider => shell,
 }
 
 service { 'nginx':
-  ensure  => 'running',
+  ensure  => running,
   require => Package['nginx'],
 }
