6270659e80ac5c0e28db20759cbfc2c75bf46cf8
faustine-van <faustinemuhayemariya44@gmail.com>
2023-11-07 21:40:45 +0200

    updated

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,24 +5,24 @@ import requests
 
 
 def recurse(subreddit, hot_list=[]):
-    """ list containing the titles of all hot articles
-	for given subreddits
+    """ list containing the titles of all hot article
+        for given subreddits
     """
     after = None
 
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     if after:
         url += f'&after{after}'
-    
     data = requests.get(url, headers=({'User-agent': 'app'}))
     if data.status_code == 200:
         info = data.json()
         for post in info['data']['children']:
             hot_list.append(post['data']['title'])
-        return (hot_list)       
-  
+
         if 'data' in data and 'after' in info['data']:
             after = info['dat']['after']
-            recurse(subreddit, hot_list)
+            return recurse(subreddit, hot_list)
+        else:
+            return (hot_list)
     else:
-        return (None)
+        return None
