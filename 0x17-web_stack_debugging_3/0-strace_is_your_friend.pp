de12106e14b2890160ce870775fd37168d7c6947
faustine-van <faustinemuhayemariya44@gmail.com>
2023-11-08 23:05:56 +0200

    puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix whay apache2 is return 500 error
 exec { 'fix 500 error':
-  command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+  command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
   provider => 'shell',
 }
